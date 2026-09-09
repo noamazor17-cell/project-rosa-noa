8a9987c70b447b33a24ca7891dc75029383afa8c
yuvalglock <youvalg2oo8@gmail.com>
2026-09-09 09:59:50 +0300

    #fix creat field\ make a move doesen't get the place of the soldier\ doesn't get e direction

--- game_field.py
+++ game_field.py
@@ -18,7 +18,7 @@ def create_field():
     return field
 
 
-def print_field(field):
+def print_field():
     #print field
     for row in field:
         for col in row:
@@ -58,7 +58,7 @@ def square_collide_with_flag():
             collide_with_flag.append((i, j))
     return collide_with_flag
 
-def square_collide_with_bomb(field):
+def square_collide_with_bomb():
     collide_with_bomb = []
     for i in range(consts.BOARD_ROWS):
         for j in range(consts.BOARD_COLS):
@@ -66,40 +66,27 @@ def square_collide_with_bomb(field):
                 collide_with_bomb.append((i, j))
     return collide_with_bomb
 
-
-def put_solider_at_field(first_squrt, solider, field):
-    soldier_i = 0
-    for i in range(first_squrt[0], first_squrt[0]+4):
-        soldier_j = 0
-        for j in range(first_squrt[1], first_squrt[1]+2):
-            field[i][j] = solider[soldier_i][soldier_j]
-            soldier_j += 1
-        soldier_i += 1
-
-
-def move(solider):
-    #return the corrct string for move
-    row = solider[0]
-    col = solider[0][0]
-    while True:
-        key = keyboard.read_key()
-        if keyboard.read_key() == 'up':
-            return [row+1, col]
-        if keyboard.read_key() == 'left':
-            return [row, col-1]
-        if keyboard.read_key() == 'right':
-            return [row, col+1]
-        if keyboard.read_key() == 'down':
-            return [row-1, col]
-
-
-def make_a_move(field, solider):
-    the_move = move(solider)
-    if soldier.is_in_board(the_move[0], the_move[1]):
-        put_solider_at_field(the_move, solider, field)
-        return True
+def is_soldier_touch_flag(soldier):
+    collide_with_flag = square_collide_with_flag()
+    for i in range (soldier[0], soldier[0]+3):
+        for j in range(soldier[1], soldier[1]+1):
+            coordinate = (i, j)
+            if coordinate in collide_with_flag:
+                return True
     else:
         return False
+
+
+def is_soldier_touch_bomb(soldier):
+    collide_with_bomb = square_collide_with_bomb(field)
+    for i in range (soldier[0]+3, soldier[0]+4):
+        for j in range (soldier[1], soldier[1]+1):
+            coordinate = (i, j)
+            if coordinate in collide_with_bomb:
+                return True
+    else:
+        return False
+
 
 
 
--- soldier.py
+++ soldier.py
@@ -45,14 +45,43 @@ def is_in_board(col, row):
     return row >= 0 and row + 3 <= consts.BOARD_ROWS and col + 1 >= consts.BOARD_COLS and col >= 0
 
 
-def print_soldier(soldier):
-    #check
-    for row in soldier:
-        for col in row:
-            print(col, end=" ")
-        print()
+# def print_soldier(soldier):
+#     #check
+#     for row in soldier:
+#         for col in row:
+#             print(col, end=" ")
+#         print()
 
 
 
-print_soldier(create_solider())
+def move():
+    #return the corrct string for move
+    row = solider[0]
+    col = solider[1]
+    print("enter arrow")
+    if keyboard.read_key() == 'up':
+        print("Up")
+        return [row+1, col]
+    elif keyboard.read_key() == 'left':
+        print("Left")
+        return [row, col-1]
+    elif keyboard.read_key() == 'right':
+        print("Right")
+        return [row, col+1]
+    elif keyboard.read_key() == 'down':
+        print("Down")
+        return [row-1, col]
+
+
+def make_a_move():
+    the_move = move()
+    if is_in_board(the_move[0], the_move[1]):
+        update_solider_location(the_move[0], the_move[1])
+        return True
+    else:
+        return False
+
+
+
+
 
